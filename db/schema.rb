d8008100087907d1f9107a2de71cf38d8c508b9b
n1ch0las <mercy.recommends@gmail.com>
2014-08-13 00:23:19 +0100

    create permissions db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811214151) do
+ActiveRecord::Schema.define(version: 20140812220400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "permissions", force: true do |t|
+    t.string   "name"
+    t.string   "profile_image_url"
+    t.string   "provider"
+    t.string   "token"
+    t.string   "token_secret"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
