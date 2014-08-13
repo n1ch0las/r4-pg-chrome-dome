85b85cc9ebe58ba91e3ad6ecce6b8840c9183714
n1ch0las <mercy.recommends@gmail.com>
2014-08-13 14:43:31 +0100

    generate Permissions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'permissions/index'
+
+  get 'permissions/create'
+
+  get 'permissions/destroy'
+
   get '/auth/:provider/callback', to: 'permissions#create'
   #resources :permissions, :only => [:index, :create, :destroy]
   devise_for :users
